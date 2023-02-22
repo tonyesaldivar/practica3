2947774a62e6ea1e400fe5547163d7d75ddbdf5d
Antonio Espinoza S <tonyesaldivar@gmail.com>
2023-02-21 20:01:43 -0600

    Mi practica 3

---
+++ app/src/main/java/com/example/application3/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.application3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void Boton1(View view)
+    {
+        Toast.makeText(this, "Boton 1", Toast.LENGTH_SHORT).show();
+    }
+    public void Boton2(View view)
+    {
+        Toast.makeText(this, "Boton 2", Toast.LENGTH_SHORT).show();
+    }
+    public void Boton3(View view)
+    {
+        Toast.makeText(this, "Boton 3", Toast.LENGTH_SHORT).show();
+    }
+    public void Boton4(View view)
+    {
+        Toast.makeText(this, "Boton 4", Toast.LENGTH_SHORT).show();
+    }
+    public void Boton5(View view)
+    {
+        Toast.makeText(this, "Boton 5", Toast.LENGTH_SHORT).show();
+    }
+    public void Boton6(View view)
+    {
+        Toast.makeText(this, "Boton 6", Toast.LENGTH_SHORT).show();
+    }
+    public void Boton7(View view)
+    {
+        Toast.makeText(this, "Boton 7", Toast.LENGTH_SHORT).show();
+    }
+    public void Boton8(View view)
+    {
+        Toast.makeText(this, "Boton 8", Toast.LENGTH_SHORT).show();
+    }
+
+
+}
